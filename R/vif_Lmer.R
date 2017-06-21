4a17406165b2c47b27d27e07ca53b122927e458b
Matt Helmus <mrhelmus@gmail.com>
2017-06-21 11:31:43 -0400

    Add vif_lmer
    Add vif lmer code, and a few edits to clean up Tyl's merge

---
+++ R/vif_Lmer.R
@@ -0,0 +1,31 @@
+#' @name vif_lmer
+#' @title VIF for lmer fit
+#' @description calculates VIF for the fixed effects in lmer fits
+#' @param fit lmer fit from \code{\link{lmer}}
+#' @return a vector of VIF values
+#' @author Jon Lefcheck see website
+# @examples None None num<
+#' @seealso \url{https://jonlefcheck.net/2012/12/28/dealing-with-multicollinearity-using-variance-inflation-factors/}
+# @references None None
+#' @rdname vif_lmer
+#' @export
+#' @importFrom lme4 fixef
+
+vif.mer <- function (fit) {
+  ## adapted from rms::vif
+  
+  v <- vcov(fit)
+  nam <- names(fixef(fit))
+  
+  ## exclude intercepts
+  ns <- sum(1 * (nam == "Intercept" | nam == "(Intercept)"))
+  if (ns > 0) {
+    v <- v[-(1:ns), -(1:ns), drop = FALSE]
+    nam <- nam[-(1:ns)]
+  }
+  
+  d <- diag(v)^0.5
+  v <- diag(solve(v/(d %o% d)))
+  names(v) <- nam
+  v
+}
