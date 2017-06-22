726ee209bf97c33c7e6704751b403cb81ec6a758
Matt Helmus <mrhelmus@gmail.com>
2017-06-22 09:53:01 -0400

    more random to adds

--- R/to_add.R
+++ R/to_add.R
@@ -24,3 +24,24 @@ flattenSquareMatrix <- function(m) {
 
 #get genera from a phylo object
 genus<-function(tree,split="_",ncol=2){matrix(unlist(strsplit(tree$tip.label,split = split)),ncol=ncol,byrow = TRUE)[,1]}
+
+
+library(ape)
+library(picante)
+library(geiger)
+phy<-read.tree("mytree.txt")
+node.age(phy)->phy.age
+cbind(phy.age$edge,phy.age$age, phy$edge.length)->BL.position
+max(phy.age$age)-BL.position[,3]->dist.tip
+cbind(BL.position,dist.tip)->BL.positions
+BL.positions[,5]+BL.positions[,4]->ages
+cbind(BL.positions,ages)->BL.positions
+as.data.frame(BL.positions)->node.ages
+names(node.ages)<-c("parental.node","daughter.node","dist.root","BL","dist.
+tip","mrca.age")
+## node.ages is a data frame listing as variables the identity of parental and
+#daughter nodes, the distance from the root and from the present of each node,
+#the branch lenght and the age of the most recent common ancestor
+node.ages[node.ages[,2]<length(phy$tip)+1,]->species.ages
+row.names(species.ages)<-phy$tip
+## species ages is node.ages data frame reduced to the tips (species)
